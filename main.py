03210b95f7bfabbd864c007c0589d4876932718b
Tiago Sievers <65292458+TiagoSievers@users.noreply.github.com>
2024-07-09 16:19:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ async def get_data(car_marca: str = Query(..., description="Marca do carro"),
                    km: Optional[str] = Query(None, description="Limite de quilometragem")):
     try:
         chrome_options = webdriver.ChromeOptions()
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
